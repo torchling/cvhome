956fc5fcb9bec47d980630b71db1f57241145398
torchling <candyrogue@gmail.com>
2016-03-31 03:54:32 +0800

    mp1_finished

--- main.cpp
+++ main.cpp
@@ -14,10 +14,14 @@ int histo[256]; //for histogram
 int histoR[256];
 int histoG[256];
 int histoB[256];
+int histoY[256];
+int histoV[256];
 int t[256];
 int tR[256];
 int tG[256];
 int tB[256];
+int tY[256];
+int tV[256];
 float p=256.0;
 
 int cnt;
@@ -33,6 +37,14 @@ int main( int argc, char **argv)
 
     IplImage *img2 = cvLoadImage( "mp1a.jpg",1);
     IplImage *rgb_img = cvCreateImage(cvGetSize(img2), IPL_DEPTH_8U, 3 );
+
+    IplImage *src2 = cvLoadImage( "mp1a.jpg",1);
+
+    IplImage *hsv = cvCreateImage(cvGetSize(src2), IPL_DEPTH_8U, 3);
+    cvCvtColor( src2, hsv, CV_BGR2HSV );
+
+    IplImage *yuv = cvCreateImage(cvGetSize(src2), IPL_DEPTH_8U, 3 );
+    cvCvtColor( src2, yuv, CV_BGR2YCrCb );
 
     double gray;
     CvScalar scalar1;
@@ -141,7 +153,69 @@ int main( int argc, char **argv)
                 if(k==scalar1.val[0])
                     b=tB[k];
             }
-            cvSet2D(rgb_img,i,j,cvScalar(r, g, b));
+            cvSet2D(rgb_img,i,j,cvScalar(b, g, r));
+        }
+    }
+
+    //--mp1a_HSV---
+    p=256.0/(src2->width*src2->height);
+
+    for(int i=0; i< src2->height;i++)
+    {
+        for(int j=0; j < src2->width; j++)
+        {
+            scalar1 = cvGet2D(src2,i,j);
+            for(int k=0;k<256;k++){
+                if(k==scalar1.val[2])
+                    histoV[k]+=1;
+            }
+        }
+    }
+    tY[0]=p*histoY[0];
+    for(int i=1; i<256; i++)
+    {
+        tV[i]=tV[i-1]+p*histoV[i];
+    }
+    for(int i=0; i< src2->height;i++)
+    {
+        for(int j=0; j < src2->width; j++)
+        {
+            scalar1 = cvGet2D(src2,i,j);
+            for(int k=0;k<256;k++){
+                if(k==scalar1.val[2])
+                    cvSet2D(hsv,i,j,cvScalar(scalar1.val[0],scalar1.val[1],tV[k]));
+            }
+        }
+    }
+
+//--mp1a_YCbCr---
+    p=256.0/(src2->width*src2->height);
+
+    for(int i=0; i< src2->height;i++)
+    {
+        for(int j=0; j < src2->width; j++)
+        {
+            scalar1 = cvGet2D(src2,i,j);
+            for(int k=0;k<256;k++){
+                if(k==scalar1.val[0])
+                    histoY[k]+=1;
+            }
+        }
+    }
+    tY[0]=p*histoY[0];
+    for(int i=1; i<256; i++)
+    {
+        tY[i]=tY[i-1]+p*histoY[i];
+    }
+    for(int i=0; i< src2->height;i++)
+    {
+        for(int j=0; j < src2->width; j++)
+        {
+            scalar1 = cvGet2D(src2,i,j);
+            for(int k=0;k<256;k++){
+                if(k==scalar1.val[0])
+                    cvSet2D(yuv,i,j,cvScalar(tY[k],scalar1.val[1],scalar1.val[2]));
+            }
         }
     }
 
@@ -151,6 +225,12 @@ int main( int argc, char **argv)
     cvNamedWindow("mp1a_RGB", 1);
     cvShowImage( "mp1a_RGB", rgb_img );
 
+    cvNamedWindow("mp1a_HSV", 1);
+    cvShowImage( "mp1a_HSV", hsv );
+
+    cvNamedWindow("mp1a_YCbCr", 1);
+    cvShowImage( "mp1a_YCbCr", yuv );
+
     cvWaitKey(0);
 
     cvReleaseImage( &grayimg );
@@ -158,6 +238,12 @@ int main( int argc, char **argv)
 
     cvReleaseImage( &rgb_img );
     cvDestroyWindow("mp1a_RGB");
+
+    cvReleaseImage( &hsv );
+    cvDestroyWindow("mp1a_HSV");
+
+    cvReleaseImage( &yuv );
+    cvDestroyWindow("mp1a_YCbCr");
 
     return 0;
 }
