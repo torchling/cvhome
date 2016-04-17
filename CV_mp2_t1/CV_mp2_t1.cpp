17e927cdc9697b8c5c7b2ae45d7798fffa6b1eba
torchling <candyrogue@gmail.com>
2016-04-17 17:53:21 +0800

    first success

--- CV_mp2_t1/CV_mp2_t1.cpp
+++ CV_mp2_t1/CV_mp2_t1.cpp
@@ -6,6 +6,38 @@
 using namespace cv;
 using namespace std;
 
+
+// Rearrange the quadrants of a Fourier image so that the origin is at
+// the image center
+void shiftDFT(Mat &fImage)
+{
+	Mat tmp, q0, q1, q2, q3;
+
+	// first crop the image, if it has an odd number of rows or columns
+
+	fImage = fImage(Rect(0, 0, fImage.cols & -2, fImage.rows & -2));
+
+	int cx = fImage.cols / 2;
+	int cy = fImage.rows / 2;
+
+	// rearrange the quadrants of Fourier image
+	// so that the origin is at the image center
+
+	q0 = fImage(Rect(0, 0, cx, cy));
+	q1 = fImage(Rect(cx, 0, cx, cy));
+	q2 = fImage(Rect(0, cy, cx, cy));
+	q3 = fImage(Rect(cx, cy, cx, cy));
+
+	q0.copyTo(tmp);
+	q3.copyTo(q0);
+	tmp.copyTo(q3);
+
+	q1.copyTo(tmp);
+	q2.copyTo(q1);
+	tmp.copyTo(q2);
+}
+
+
 int main(int argc, char ** argv)
 {
 	const char* filename = argc >= 2 ? argv[1] : "woman.jpg";
@@ -14,69 +46,36 @@ int main(int argc, char ** argv)
 	if (I.empty())
 		return -1;
 
-	Mat padded;                            //expand input image to optimal size
+	Mat padded;                            
+	//expand input image to optimal size
 	int m = getOptimalDFTSize(I.rows); 
-	int n = getOptimalDFTSize(I.cols); // on the border add zero values
+	int n = getOptimalDFTSize(I.cols);
+	
+	// on the border add zero values
 	copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
 
 	Mat planes[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) };
+	Mat complexI;
+
+	merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+	dft(complexI, complexI);            // this way the result may fit in the source matrix
+	
 	Mat planes_mp[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) }; //planes_mp[0] is for magnitude, planes_mp[1] is for phase
 	Mat recon[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) };
-	Mat complexI;
-	merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
 
-	dft(complexI, complexI);            // this way the result may fit in the source matrix
-
-	// compute the magnitude and switch to logarithmic scale
-	// => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
-	split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+	split(complexI, planes);            // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
 	
-	magnitude(planes[0], planes[1], planes_mp[0]);// planes_mp[0] = magnitude
-	phase(planes[0], planes[1], planes_mp[1]);//jcrs_01
-		
-	Mat magI = planes_mp[0];
-	Mat phaI = planes_mp[1];
 
-	magI += Scalar::all(1);                    // switch to logarithmic scale
-	log(magI, magI);
-	
-	// crop the spectrum, if it has an odd number of rows or columns
-	magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
-
-	// rearrange the quadrants of Fourier image  so that the origin is at the image center
-	int cx = magI.cols / 2;
-	int cy = magI.rows / 2;
-
-	Mat q0(magI, Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-	Mat q1(magI, Rect(cx, 0, cx, cy));  // Top-Right
-	Mat q2(magI, Rect(0, cy, cx, cy));  // Bottom-Left
-	Mat q3(magI, Rect(cx, cy, cx, cy)); // Bottom-Right
-
-	Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
-	q0.copyTo(tmp);
-	q3.copyTo(q0);
-	tmp.copyTo(q3);
-
-	q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
-	q2.copyTo(q1);
-	tmp.copyTo(q2);
-
-	normalize(magI, magI, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
-	// viewable image form (float between values 0 and 1).
-
-	normalize(phaI, phaI, 0, 1, CV_MINMAX);
-	
+	cartToPolar(planes[0], planes[1], planes_mp[0], planes_mp[1]);
 	polarToCart(planes_mp[0], planes_mp[1], recon[0], recon[1]);
 
 	Mat recI;
 	merge(recon, 2, recI);
 	Mat inverseTransform;
 	dft(recI, inverseTransform, DFT_INVERSE | DFT_REAL_OUTPUT);
-	//normalize(inverseTransform, inverseTransform, 0, 1, CV_MINMAX);
+	normalize(inverseTransform, inverseTransform, 0, 1, CV_MINMAX);
 	
 	imshow("Input Image", I);    // Show the result
-	imshow("spectrum magnitude", magI);
-	imshow("phase", phaI);
 	imshow("Reconstructed", inverseTransform);
 	
 	waitKey();
