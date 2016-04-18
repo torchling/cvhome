c89ec21e197c9ed56747b809120122f4bf344d0c
torchling <candyrogue@gmail.com>
2016-04-18 16:16:24 +0800

    mp2_done

--- CV_mp2_t1/CV_mp2_t1.cpp
+++ CV_mp2_t1/CV_mp2_t1.cpp
@@ -7,8 +7,6 @@ using namespace cv;
 using namespace std;
 
 
-// Rearrange the quadrants of a Fourier image so that the origin is at
-// the image center
 void shiftDFT(Mat &fImage)
 {
 	Mat tmp, q0, q1, q2, q3;
@@ -40,43 +38,77 @@ void shiftDFT(Mat &fImage)
 
 int main(int argc, char ** argv)
 {
-	const char* filename = argc >= 2 ? argv[1] : "woman.jpg";
+	const char* filename1 = argc >= 2 ? argv[1] : "woman.jpg";//"john.jpg";
+	const char* filename2 = argc >= 2 ? argv[1] : "rectangle.jpg";//"mow.jpg";
 
-	Mat I = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+	Mat I = imread(filename1, CV_LOAD_IMAGE_GRAYSCALE);
+	if (I.empty())
+		return -1;
+	
+	Mat I2 = imread(filename2, CV_LOAD_IMAGE_GRAYSCALE);
 	if (I.empty())
 		return -1;
 
+	/*	
 	Mat padded;                            
 	//expand input image to optimal size
 	int m = getOptimalDFTSize(I.rows); 
 	int n = getOptimalDFTSize(I.cols);
-	
 	// on the border add zero values
 	copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
+	*/
 
-	Mat planes[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) };
+	// image 01
+	Mat planes[] = { Mat_<float>(I), Mat::zeros(I.size(), CV_32F) };
 	Mat complexI;
-
 	merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
-	dft(complexI, complexI);            // this way the result may fit in the source matrix
-	
-	Mat planes_mp[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) }; //planes_mp[0] is for magnitude, planes_mp[1] is for phase
-	Mat recon[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) };
 
+	dft(complexI, complexI);            // FourierTransform
 	split(complexI, planes);            // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-	
 
+	Mat planes_mp[] = { Mat_<float>(I), Mat::zeros(I.size(), CV_32F) }; //planes_mp[0] is for magnitude, planes_mp[1] is for phase
+	
 	cartToPolar(planes[0], planes[1], planes_mp[0], planes_mp[1]);
-	polarToCart(planes_mp[0], planes_mp[1], recon[0], recon[1]);
 
-	Mat recI;
-	merge(recon, 2, recI);
-	Mat inverseTransform;
-	dft(recI, inverseTransform, DFT_INVERSE | DFT_REAL_OUTPUT);
-	normalize(inverseTransform, inverseTransform, 0, 1, CV_MINMAX);
+	/*-------------------*/
+
+	// image 02
+	Mat planes2[] = { Mat_<float>(I2), Mat::zeros(I2.size(), CV_32F) };
+
+	merge(planes2, 2, complexI);         // Add to the expanded another plane with zeros
+
+	dft(complexI, complexI);            // FourierTransform
+	split(complexI, planes2);            // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
 	
-	imshow("Input Image", I);    // Show the result
-	imshow("Reconstructed", inverseTransform);
+	Mat planes_mp2[] = { Mat_<float>(I2), Mat::zeros(I2.size(), CV_32F) }; //planes_mp[0] is for magnitude, planes_mp[1] is for phase
+	
+	cartToPolar(planes2[0], planes2[1], planes_mp2[0], planes_mp2[1]);
+
+	/*-------------------*/
+
+	Mat recon[] = { Mat_<float>(I), Mat::zeros(I.size(), CV_32F) };
+	Mat recon2[] = { Mat_<float>(I2), Mat::zeros(I2.size(), CV_32F) };
+
+	polarToCart(planes_mp2[0], planes_mp[1], recon[0], recon[1]);
+	polarToCart(planes_mp[0], planes_mp2[1], recon2[0], recon2[1]);
+
+	Mat recI;   merge(recon, 2, recI);
+	Mat recI2;  merge(recon2, 2, recI2);
+
+	Mat inverseTransform;
+	Mat inverseTransform2;
+
+	dft(recI, inverseTransform, DFT_INVERSE | DFT_REAL_OUTPUT);
+	dft(recI2, inverseTransform2, DFT_INVERSE | DFT_REAL_OUTPUT);
+	
+	normalize(inverseTransform, inverseTransform, 0, 1, CV_MINMAX);
+	normalize(inverseTransform2, inverseTransform2, 0, 1, CV_MINMAX);
+
+	imshow("Input Image", I);   
+	imshow("Input Image2", I2);
+	// Show the result
+	imshow("Result1", inverseTransform);
+	imshow("Result2", inverseTransform2);
 	
 	waitKey();
 
